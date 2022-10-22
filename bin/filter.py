1fe783fca0732579d54d7501a2e088249542e62d
tck199732 <ctb8135@wmich.edu>
2022-10-22 11:14:52 -0400

    add amd2root

--- bin/filter.py
+++ bin/filter.py
@@ -1,36 +1,55 @@
-from pickle import TRUE
 import subprocess
 import pathlib
 import os
+import itertools
 
+anal_date = 'dec2021'
 env_dir = pathlib.Path(os.environ['CONDA_PREFIX'])
 project_dir = env_dir.parent.resolve()
+database = pathlib.Path(project_dir, f'database')
 exe_dir = pathlib.Path(project_dir, 'bin')
-# out_dir = pathlib.Path(project_dir, 'result')
-out_dir = pathlib.Path('/data/AMD/filtered')
+src_dir = pathlib.Path(project_dir, 'src')
+out_dir = pathlib.Path(f'/data/AMD/filtered/{anal_date}')
 out_dir.mkdir(exist_ok=True)
 
 beam = 'Ca'
-beamA = 40
+beamA = 48
 target = 'Ni'
-targetA = 58
+targetA = 64
 energy = 140
 skyrme = 'SkM'
+mode = "21"
+mode = "3"
+##############################################################################
+# beam = ['Ca40', 'Ca48']
+# target = ['Ni58', 'Ni64', 'Sn112', 'Sn124']
+# energy = [56,140]
+# mode = ["21", "3"]
+##############################################################################
 
 
 def main():
     reaction = f'{beam}{beamA}{target}{targetA}E{energy}'
-    mode = '21'
     path_list = pathlib.Path(
-        exe_dir, f'filelist/dec2021/{beam}{beamA}{target}{targetA}_En{energy}MeV_{skyrme}.list')
+        exe_dir, f'{str(database)}/inputlist/dec2021/{beam}{beamA}{target}{targetA}En{energy}MeV_{skyrme}.list')
 
     path_out = pathlib.Path(
         out_dir, f'{beam}{beamA}{target}{targetA}_En{energy}MeV_{skyrme}_table{mode}.root')
 
-    os.chdir(exe_dir)
+    filter = pathlib.Path(exe_dir, 'ExpFilter')
+    if not filter.exists():
+        os.chdir(exe_dir)
+        subprocess.run(
+            f'g++ ExpFilter.cpp -o ExpFilter -I`root-config --cflags --libs --glibs` -I{str(src_dir)}', shell=True)
+
+    print('reaction : ', reaction)
+    print('mode : ', mode)
+    print(str(path_list))
+    print(str(path_out))
+    print(str(filter))
 
     subprocess.run(
-        f'a {reaction} {mode} {path_list} {path_out}', shell=True, text=True)
+        f'{str(filter)} {reaction} {mode} {path_list} {path_out}', shell=True, text=True)
 
 
 if __name__ == '__main__':
---
+++ bin/multiplicity.cpp
@@ -0,0 +1,169 @@
+
+
+#include "ExpFilter.hh"
+#include "TFile.h"
+struct histograms
+{
+    std::string reaction, mode;
+    TH2D *h2_multi_b;
+    void init();
+    void fill(const event &event);
+    void write();
+};
+
+void histograms::init()
+{
+    this->h2_multi_b = new TH2D(
+        Form("h2_multi_b_%s", this->mode.c_str()), Form("h2_multi_b_%s", this->mode.c_str()), 35, -0.5, 34.5, 1000, 0., 10.);
+    this->h2_multi_b->SetDirectory(0);
+}
+void histograms::write()
+{
+    this->h2_multi_b->Write();
+}
+
+void histograms::fill(const event &event)
+{
+    this->h2_multi_b->Fill(event.Nc, event.bimp);
+}
+
+struct manager
+{
+    std::string reaction, mode;
+    fs::path path_list;
+    fs::path path_out;
+
+    std::vector<fs::path> data_paths;
+    system_info *sys_info;
+    double betacms, rapidity_beam;
+
+    RootReader *reader;
+    histograms histograms;
+    TFile *outputfile;
+
+    void init();
+    void read();
+    void fill(const event &event);
+    void finish();
+};
+
+void main(int argc, char *argv[])
+{
+
+    std::string reaction = argv[1];
+    std::string mode = argv[2];
+    std::string path_list = argv[3];
+    std::string path_out = argv[4];
+    manager manager = {reaction, mode, path_list, path_out};
+    manager.init();
+    manager.read();
+    manager.finish();
+}
+
+void manager::init()
+{
+    std::string pth_name;
+    std::ifstream pth_stream(fs::absolute(this->path_list));
+    while (pth_stream >> pth_name)
+    {
+        fs::path pth = DIR_DATA / (pth_name + "_table" + this->mode + ".root");
+        std::cout << pth.string();
+        if (fs::exists(pth))
+        {
+            std::cout << " exist" << std::endl;
+            this->data_paths.push_back(pth);
+        }
+        else
+        {
+            std::cout << " does not exist" << std::endl;
+        }
+    };
+
+    std::vector<branch> branches = {
+        {"multi", "int"},
+        {"BSim", "double"},
+        {"pxCMS", "double[]"},
+        {"pyCMS", "double[]"},
+        {"pzCMS", "double[]"},
+        {"N", "int[]"},
+        {"Z", "int[]"},
+    };
+
+    for (auto &br : branches)
+    {
+        br.autofill();
+    }
+
+    this->reader = new RootReader("AMD");
+    for (auto &pth : this->data_paths)
+    {
+        reader->add_file(fs::absolute(pth));
+    }
+    reader->set_branches(branches);
+
+    this->sys_info = new system_info();
+    this->betacms = sys_info->get_betacms(this->reaction);
+    this->rapidity_beam = sys_info->get_rapidity_beam(this->reaction);
+
+    histograms.init();
+}
+
+void manager::read()
+{
+    int nevents = this->reader->tree->GetEntries();
+    std::cout << "number of events = " << nevents << std::endl;
+
+    int Nc;
+    double bimp;
+    int *fz;
+    int *fn;
+    double *px;
+    double *py;
+    double *pz;
+
+    std::vector<particle> particles;
+    for (int ievt = 0; ievt < nevents; ievt++)
+    {
+        std::map<std::string, std::any> map = this->reader->get_entry(ievt);
+        try
+        {
+            Nc = std::any_cast<int>(map["multi"]);
+            bimp = std::any_cast<double>(map["BSim"]);
+            fn = std::any_cast<int *>(map["N"]);
+            fz = std::any_cast<int *>(map["Z"]);
+            px = std::any_cast<double *>(map["pxCMS"]);
+            py = std::any_cast<double *>(map["pyCMS"]);
+            pz = std::any_cast<double *>(map["pzCMS"]);
+        }
+
+        catch (const std::bad_any_cast &e)
+        {
+            std::cout << e.what() << '\n';
+        }
+
+        for (unsigned int i = 0; i < ; i++)
+        {
+            particle particle{fn[i], fz[i], px[i], py[i], pz[i]};
+            particle.autofill(this->betacms);
+            particles.push_back(particle);
+        }
+
+        event event = {Nc, bimp, particles};
+
+        this->fill(event);
+        particles.clear();
+    }
+}
+
+void manager::fill(const event &event)
+{
+    this->histograms.fill(event);
+}
+
+void manager::finish()
+{
+    this->outputfile = new TFile(fs::absolute(this->path_out), "RECREATE");
+    this->histograms->write();
+    this->outputfile->write();
+    std::cout << "DONE" << std::endl;
+}
