62b2ac96983c7149720108dee81406d6e98e55e8
tck199732 <ctb8135@wmich.edu>
2022-10-24 19:33:41 -0400

    add cpp program for spectra anal

--- bin/spectra.cpp
+++ bin/spectra.cpp
@@ -1,96 +1,84 @@
 
-
-#include "ExpFilter.hh"
-#include "TFile.h"
-#include "TH2D.h"
-struct histograms
-{
-    std::string reaction, mode;
-    std::vector<std::string> particlenames;
-
-    std::map<std::string, TH2D *> h2_pta_rapidity_lab_all;
-
-    void init();
-    void fill(const event &event);
-    void write();
-};
-
-void histograms::init()
-{
-
-    for (const auto &pn : this->particlenames)
-    {
-        this->
-    }
-}
-void histograms::write()
-{
-    this->h2_multi_b->Write();
-}
-
-void histograms::fill(const event &event)
-{
-    this->h2_multi_b->Fill(event.Nc, event.bimp);
-}
+#include "spectra.hh"
 
 struct manager
 {
-    std::string reaction, mode;
-    fs::path path_list;
+    std::string reaction;
+    fs::path path_data21;
+    fs::path path_data3;
     fs::path path_out;
-
-    std::vector<fs::path> data_paths;
+    int Ncmin, Ncmax;
+    double bmin, bmax;
     system_info *sys_info;
     double betacms, rapidity_beam;
 
-    RootReader *reader;
-    histograms histograms;
+    RootReader *reader21;
+    RootReader *reader3;
+    histograms hist21;
+    histograms hist3;
+    histograms hist3_ndecay1;
+    eventcut eventcut;
     TFile *outputfile;
 
     void init();
     void read();
-    void fill(const event &event);
+    void replace_error();
     void finish();
 };
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-
     std::string reaction = argv[1];
-    std::string mode = argv[2];
-    std::string path_list = argv[3];
+    std::string path_data21 = argv[2];
+    std::string path_data3 = argv[3];
     std::string path_out = argv[4];
-    manager manager = {reaction, mode, path_list, path_out};
+    int Ncmin = 1;
+    int Ncmax = 25;
+    double bmin = 0.0;
+    double bmax = 0.0;
+    if (argc <= 6)
+    {
+        Ncmin = std::stoi(argv[5]);
+    }
+    if (argc <= 7)
+    {
+        Ncmax = std::stoi(argv[6]);
+    }
+    if (argc <= 8)
+    {
+        Ncmin = std::stod(argv[7]);
+    }
+    if (argc <= 9)
+    {
+        Ncmin = std::stod(argv[8]);
+    }
+
+    manager manager = {reaction, path_data21, path_data3, path_out};
     manager.init();
     manager.read();
+    manager.replace_error();
     manager.finish();
+    return 0;
 }
 
 void manager::init()
 {
-    std::string pth_name;
-    std::ifstream pth_stream(fs::absolute(this->path_list));
-    while (pth_stream >> pth_name)
+    if (!fs::exists(this->path_data21))
     {
-        fs::path pth = DIR_DATA / (pth_name + "_table" + this->mode + ".root");
-        std::cout << pth.string();
-        if (fs::exists(pth))
-        {
-            std::cout << " exist" << std::endl;
-            this->data_paths.push_back(pth);
-        }
-        else
-        {
-            std::cout << " does not exist" << std::endl;
-        }
-    };
+        throw std::invalid_argument("path_data21 does not exist.");
+    }
+    if (!fs::exists(this->path_data3))
+    {
+        throw std::invalid_argument("path_data3 does not exist.");
+    }
 
     std::vector<branch> branches = {
+        {"Nc", "int"},
         {"multi", "int"},
-        {"BSim", "double"},
-        {"pxCMS", "double[]"},
-        {"pyCMS", "double[]"},
-        {"pzCMS", "double[]"},
+        {"b", "double"},
+        {"px", "double[]"},
+        {"py", "double[]"},
+        {"pz", "double[]"},
         {"N", "int[]"},
         {"Z", "int[]"},
     };
@@ -100,46 +88,102 @@ void manager::init()
         br.autofill();
     }
 
-    this->reader = new RootReader("AMD");
-    for (auto &pth : this->data_paths)
-    {
-        reader->add_file(fs::absolute(pth));
-    }
-    reader->set_branches(branches);
+    this->reader21 = new RootReader("AMD");
+    reader21->add_file(fs::absolute(this->path_data21));
+    reader21->set_branches(branches);
+    this->reader3 = new RootReader("AMD");
+    reader3->add_file(fs::absolute(this->path_data3));
+    reader3->set_branches(branches);
 
     this->sys_info = new system_info();
     this->betacms = sys_info->get_betacms(this->reaction);
     this->rapidity_beam = sys_info->get_rapidity_beam(this->reaction);
 
-    histograms.init();
+    this->eventcut = {{this->Ncmin, this->Ncmax}, {this->bmin, this->bmax}};
+
+    this->hist21 = {this->reaction, "prim"};
+    this->hist3 = {this->reaction, "seq"};
+    this->hist3_ndecay1 = {this->reaction, "seq1"};
+
+    this->hist21.init();
+    this->hist3.init();
+    this->hist3_ndecay1.init();
 }
 
 void manager::read()
 {
-    int nevents = this->reader->tree->GetEntries();
-    std::cout << "number of events = " << nevents << std::endl;
+    int nevents21 = this->reader21->tree->GetEntries();
+    int nevents3 = this->reader21->tree->GetEntries();
+    std::cout << "number of events = " << nevents21 << std::endl;
 
-    int Nc;
+    int decays = nevents3 / nevents21;
+
+    int Nc, multi;
     double bimp;
     int *fz;
     int *fn;
     double *px;
     double *py;
     double *pz;
-
     std::vector<particle> particles;
-    for (int ievt = 0; ievt < nevents; ievt++)
+
+    for (int ievt21 = 0; ievt21 < nevents21; ievt21++)
     {
-        std::map<std::string, std::any> map = this->reader->get_entry(ievt);
+        double weight;
+        for (int ndecay = 0; ndecay < 10; ndecay++)
+        {
+            int ievt3 = ievt21 + ndecay * nevents21;
+            std::map<std::string, std::any> map = this->reader3->get_entry(ievt3);
+            try
+            {
+                Nc = std::any_cast<int>(map["Nc"]);
+                multi = std::any_cast<int>(map["multi"]);
+                bimp = std::any_cast<double>(map["b"]);
+                fn = std::any_cast<int *>(map["N"]);
+                fz = std::any_cast<int *>(map["Z"]);
+                px = std::any_cast<double *>(map["px"]);
+                py = std::any_cast<double *>(map["py"]);
+                pz = std::any_cast<double *>(map["pz"]);
+            }
+
+            catch (const std::bad_any_cast &e)
+            {
+                std::cout << e.what() << '\n';
+            }
+
+            for (unsigned int i = 0; i < multi; i++)
+            {
+                particle particle{fn[i], fz[i], px[i], py[i], pz[i]};
+                particle.autofill(this->betacms);
+                particles.push_back(particle);
+            }
+
+            event event = {Nc, bimp, particles};
+            if (this->eventcut.pass(event))
+            {
+                weight += 1.;
+                if (ndecay == 0)
+                {
+                    this->hist3_ndecay1.fill(event, 1.);
+                    this->hist3_ndecay1.norm += 1.;
+                }
+                this->hist3.fill(event, 1. / decays);
+                this->hist3.norm += 1.;
+            }
+            particles.clear();
+        }
+        weight /= decays;
+        std::map<std::string, std::any> map = this->reader21->get_entry(ievt21);
         try
         {
-            Nc = std::any_cast<int>(map["multi"]);
-            bimp = std::any_cast<double>(map["BSim"]);
+            Nc = std::any_cast<int>(map["Nc"]);
+            multi = std::any_cast<int>(map["multi"]);
+            bimp = std::any_cast<double>(map["b"]);
             fn = std::any_cast<int *>(map["N"]);
             fz = std::any_cast<int *>(map["Z"]);
-            px = std::any_cast<double *>(map["pxCMS"]);
-            py = std::any_cast<double *>(map["pyCMS"]);
-            pz = std::any_cast<double *>(map["pzCMS"]);
+            px = std::any_cast<double *>(map["px"]);
+            py = std::any_cast<double *>(map["py"]);
+            pz = std::any_cast<double *>(map["pz"]);
         }
 
         catch (const std::bad_any_cast &e)
@@ -147,7 +191,7 @@ void manager::read()
             std::cout << e.what() << '\n';
         }
 
-        for (unsigned int i = 0; i < ; i++)
+        for (unsigned int i = 0; i < multi; i++)
         {
             particle particle{fn[i], fz[i], px[i], py[i], pz[i]};
             particle.autofill(this->betacms);
@@ -155,21 +199,39 @@ void manager::read()
         }
 
         event event = {Nc, bimp, particles};
-
-        this->fill(event);
+        if (this->eventcut.pass(event))
+        {
+            this->hist21.fill(event, weight);
+            this->hist21.norm += weight;
+        }
         particles.clear();
     }
 }
 
-void manager::fill(const event &event)
+void manager::replace_error()
 {
-    this->histograms.fill(event);
+    for (auto &[pn, h2] : this->hist3.h2_pta_rapidity_lab)
+    {
+        for (int i = 0; i < h2->GetNbinsX(); i++)
+        {
+            for (int j = 0; j < h2->GetNbinsY(); j++)
+            {
+                double error = this->hist3_ndecay1.h2_pta_rapidity_lab[pn]->GetBinError(i + 1, j + 1);
+                h2->SetBinError(i + 1, j + 1, error);
+            }
+        }
+    }
 }
 
 void manager::finish()
 {
-    this->outputfile = new TFile(fs::absolute(this->path_out), "RECREATE");
-    this->histograms->write();
-    this->outputfile->write();
+    this->outputfile = new TFile(this->path_out.c_str(), "RECREATE");
+    this->hist21.normalize();
+    this->hist3.normalize();
+    this->hist3_ndecay1.normalize();
+    this->hist21.write();
+    this->hist3.write();
+    this->hist3_ndecay1.write();
+    this->outputfile->Write();
     std::cout << "DONE" << std::endl;
 }
---
+++ bin/spectra.hh
@@ -0,0 +1,89 @@
+#include "TFile.h"
+#include "TH2D.h"
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+#include "../src/particle.cpp"
+#include "../src/root_io.cpp"
+#include "../src/system_info.cpp"
+
+struct event
+{
+    int Nc;
+    double bimp;
+    std::vector<particle> particles;
+};
+
+struct eventcut
+{
+    std::array<int, 2> Nccut;
+    std::array<double, 2> bcut;
+    bool pass(const event &event)
+    {
+        return (event.Nc >= this->Nccut[0] && event.Nc <= this->Nccut[1] && event.bimp >= this->bcut[0] && event.bimp < this->bcut[1]);
+    }
+};
+
+struct histograms
+{
+    std::string reaction, tag;
+    std::vector<std::string> particlenames = {
+        "n", "p", "d", "t", "3He", "4He", "coal_n", "coal_p"};
+
+    double norm = 0.0;
+    std::map<std::string, TH2D *> h2_pta_rapidity_lab;
+    void init();
+    void fill(const event &event, const std::string &mode, const double &weight);
+    // void replace_error();
+    void write();
+};
+
+void histograms::init()
+{
+    for (const auto &pn : this->particlenames)
+    {
+        std::string hname = "h2_pta_rapidity_lab_" + this->tag + "_" + pn;
+        this->h2_pta_rapidity_lab[pn] = new TH2D(hname.c_str(), "", 100, 0., 1., 600, 0, 600);
+        this->h2_pta_rapidity_lab[pn]->Sumw2();
+        this->h2_pta_rapidity_lab[pn]->SetDirectory(0);
+    }
+}
+
+void histograms::write()
+{
+    for (const auto &pn : this->particlenames)
+    {
+        this->h2_pta_rapidity_lab[pn]->Write();
+    }
+}
+
+void histograms::normalize()
+{
+    for (const auto &pn : this->particlenames)
+    {
+        this->h2_pta_rapidity_lab[pn]->Scale(1. / this->norm);
+    }
+}
+
+void histograms::fill(const event &event, const double &weight)
+{
+    for (auto &par : event.particles)
+    {
+        if (this->h2_pta_rapidity_lab.count(par.name) == 1)
+        {
+            this->h2_pta_rapidity_lab[par.name]->Fill(par.rapidity_lab, par.pt, weight);
+
+            if (h2_pta_rapidity_lab.count("coal_p") == 1)
+            {
+                this->h2_pta_rapidity_lab["coal_p"]->Fill(par.rapidity_lab / this->beam_rapidity, par.pt, weight * par.zid);
+            }
+            if (h2_pta_rapidity_lab.count("coal_n") == 1)
+            {
+                this->h2_pta_rapidity_lab["coal_n"]->Fill(par.rapidity_lab / this->beam_rapidity, par.pt, weight * par.nid);
+            }
+        }
+    }
+}
