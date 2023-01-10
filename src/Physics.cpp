29c4dad6ad966b20ddfff8877b9c39c4cd6334f9
tck199732 <ctb8135@wmich.edu>
2023-01-10 00:04:51 -0500

    ✨add simple phy calculations

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -177,3 +177,62 @@ double Physics::GetBeamRapidity(const std::string &beam, const int &beamEperA, c
 
     return 0.5 * TMath::Log((beam_energy_tot + mom_beam) / (beam_energy_tot - mom_beam));
 }
+
+float Physics::GetPt(const float &px, const float &py)
+{
+    return TMath::Sqrt(pow(px, 2.) + pow(py, 2.));
+}
+
+float Physics::GetP(const float &pt, const float &pz)
+{
+    return TMath::Sqrt(pow(pt, 2.) + pow(pz, 2.));
+}
+
+float Physics::GetP(const float &px, const float &py, const float &pz)
+{
+    return Physics::GetP(Physics::GetPt(px, py), pz);
+}
+
+float Physics::GetEkin(const float &mass, const float &p)
+{
+    return TMath::Sqrt(pow(p, 2.) + pow(mass, 2.)) - mass;
+}
+
+float Physics::GetEkin(const float &mass, const float &px, const float &py,
+                       const float &pz)
+{
+    float p = Physics::GetP(px, py, pz);
+    return Physics::GetEkin(mass, p);
+}
+
+float Physics::GetPhi(const float &px, const float &py)
+{
+    return TMath::ATan2(py, px);
+}
+
+float Physics::GetTheta(const float &pt, const float &pz)
+{
+    return TMath::ATan2(pt, pz);
+}
+
+/**
+ * @brief Boost from lab framm to cms frame if betacms is positive
+ *
+ * @param mass
+ * @param pz
+ * @param ekin
+ * @param betacms
+ * @return float
+ */
+float Physics::boostz(const float &mass, const float &pz, const float &ekin,
+                      const float &betacms)
+{
+    float gamma = 1. / TMath::Sqrt(1 - pow(betacms, 2.));
+    return gamma * (pz - betacms * (ekin + mass));
+}
+
+float Physics::GetRapidity(const float &ekin, const float &pz,
+                           const float &mass)
+{
+    return 0.5 * TMath::Log((ekin + pz + mass) / (ekin - pz + mass));
+}
--- src/Physics.hh
+++ src/Physics.hh
@@ -28,6 +28,17 @@ namespace Physics
     double GetNucleiMass(const std::string &symbol, const std::string &option = "new");
     double GetNucleiMass(const int &Z, const int &A, const std::string &option = "new");
 
+    // general physics
+    float GetPt(const float &px, const float &py);
+    float GetP(const float &pt, const float &pz);
+    float GetP(const float &px, const float &py, const float &pz);
+    float GetEkin(const float &mass, const float &px, const float &py, const float &pz);
+    float GetEkin(const float &mass, const float &p);
+    float GetPhi(const float &px, const float &py);
+    float GetTheta(const float &pt, const float &pz);
+    float boostz(const float &mass, const float &pz, const float &ekin, const float &betacms);
+    float GetRapidity(const float &ekin, const float &pz, const float &mass);
+
 };
 
 #endif
