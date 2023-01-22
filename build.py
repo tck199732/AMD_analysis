f442a1a14b433fc58e1c3decebd857371f21f271
tck199732 <ctb8135@wmich.edu>
2023-01-22 13:35:32 -0500

    ⬇️ dependency rebuild

--- build.py
+++ build.py
@@ -3,65 +3,72 @@ import sys
 if not (sys.version_info[0] >= 3 and sys.version_info[1] >= 7):
     raise Exception('Requires at least Python 3.7 to use this repository.')
 
-import glob
-import inspect
-import os
 import pathlib
 import subprocess
+import inspect
+import glob
 
-ENVIRONMENT_NAME = 'env_amd'
+# env_name is used only as the directory name of the env 
+env_name = 'env_amd' 
+project_dir = pathlib.Path(__file__).parent.resolve()
+env_yml = pathlib.Path(project_dir, 'environment.yml')
+env_dir = pathlib.Path(project_dir, env_name)
 
 def main():
+    
     check_conda_activated()
 
-    # create a conda environment locally
-    subprocess.run(f'conda env create --prefix ./{ENVIRONMENT_NAME} -f environment.yml', shell=True)
-
-    # add packages in this project/repository to (editable) site-packages of this conda environment
-    site_pkgs_path = glob.glob(f'./{ENVIRONMENT_NAME}/lib/python*/site-packages')[0]
-    path = pathlib.Path(site_pkgs_path, 'conda.pth').resolve()
-    project_dir = pathlib.Path(__file__).parent.resolve()
-    with open(path, 'w') as file:
-        file.write(str(project_dir) + '\n')
-
-    # add activation and deactivation scripts
-    env_vars_content = {
-        'activate': inspect.cleandoc(f'''
-            #!/bin/bash
-            if [ ! -z "$PROJECT_DIR" ]; then
-                export OLD_PROJECT_DIR=$PROJECT_DIR
-            fi
-            export PROJECT_DIR="{str(pathlib.Path('.').resolve())}"
-        '''),
-        'deactivate': inspect.cleandoc(f'''
-            #!/bin/bash
-            if [ ! -z "$OLD_PROJECT_DIR" ]; then
-                PROJECT_DIR=$OLD_PROJECT_DIR
-                unset OLD_PROJECT_DIR
-            else
-                unset PROJECT_DIR
-            fi
-        '''),
-    }
-    src_parent_dir = pathlib.Path('./local/etc/conda')
-    des_parent_dir = pathlib.Path(ENVIRONMENT_NAME, 'etc/conda')
-    base_fname = 'env_vars.sh'
-    for name in ['activate', 'deactivate']:
-        src_subdir = pathlib.Path(src_parent_dir, f'{name}.d')
-        src_subdir.mkdir(parents=True, exist_ok=True)
-        src_path = pathlib.Path(src_subdir, f'{name}-{base_fname}')
-        src_path.touch(exist_ok=True)
-        with open(src_path, 'w') as file:
-            file.write(env_vars_content[name] + '\n')
-
-        des_subdir = pathlib.Path(des_parent_dir, f'{name}.d')
-        des_subdir.mkdir(parents=True, exist_ok=True)
-        des_path = pathlib.Path(des_subdir, f'{name}-{base_fname}')
-        if des_path.is_symlink():
-            des_path.unlink()
-        des_path.resolve().symlink_to(src_path.resolve())
+    # create conda environment
+    subprocess.run(f'conda env create -f {env_yml} --prefix {env_dir}', shell=True, text=True)
     
-    print('Done!')
+    # create script to save environment variables
+    env_vars = {
+        'PROJECT_DIR' : str(project_dir),
+    }
+
+    scripts = {
+
+    'activate' : [inspect.cleandoc(f"""
+        if [ ! -z '${name}' ]; then
+            export OLD_{name}=${name};
+        fi
+        export {name}='{value}'
+    """) for name, value in env_vars.items()],
+    
+    'deactivate' : [inspect.cleandoc(f"""
+        if [ ! -z '$OLD_{name}' ]; then
+            {name}='$OLD_{name}'
+            unset OLD_{name}
+        fi
+    """) for name, value in env_vars.items()]
+
+    }
+    
+    local_dir = pathlib.Path(project_dir, 'local')
+    for name in ['activate', 'deactivate']:
+
+        src_dir = pathlib.Path(local_dir, f'etc/conda/{name}.d')
+        src_dir.mkdir(exist_ok=True, parents=True)
+        content = '\n'.join(['#!/bin/bash'] + scripts[name])
+        src_pth = pathlib.Path(src_dir, 'env_vars.sh')
+
+        with open(str(src_pth), 'w') as f:
+            f.write(content + '\n')
+        
+        target_dir =  pathlib.Path(env_dir, f'etc/conda/{name}.d')
+        target_dir.mkdir(exist_ok=True, parents=True)
+        target_pth = pathlib.Path(target_dir, 'env_vars.sh')
+        if target_pth.is_symlink():
+            target_pth.unlink()
+        target_pth.symlink_to(src_pth)
+        
+    # add the project home directory to conda.pth
+    pths = glob.glob(f'{str(env_dir)}/lib/python*/site-packages')
+    for pth in pths:
+        pth = pathlib.Path(pth, 'conda.pth')
+        with open(str(pth), 'w') as f:
+            f.write(str(project_dir) + '\n')
+
 
 def check_conda_activated():
     which_conda = subprocess.run('which conda', shell=True, capture_output=True, text=True)
