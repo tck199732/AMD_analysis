a26b8ecf18c359d91c3d4d6da63559b55f160109
tck199732 <ctb8135@wmich.edu>
2023-01-25 22:04:01 -0500

    ⚡️iminuit now, much faster

--- pyamd/analysis/isoscaling.py
+++ pyamd/analysis/isoscaling.py
@@ -1,47 +1,135 @@
 import pandas as pd
 import numpy as np
+from typing import Literal
 import iminuit
+
 from pyamd.e15190 import e15190
 from pyamd.utilities import dataframe, minuit
 df_helper = dataframe.DataFrameHelper()
 
-
 class Isoscaling:
-    def __init__(self, reaction1, reaction2):
-        self.reaction1 = reaction1
-        self.reaction2 = reaction2
+    def __init__(self):
         self.r21 = dict()
+        # minuit attributes to be stored later
+        self.is_fitted = False
+        self.chi2 = None
+        self.param_names = None
+        self.param_values = dict()
+        self.param_errors = dict()
 
-    def SetR21(self, particle, df):
-        self.r21[particle] = df
-
-    def _model(self, Z, N, norm, alpha, beta):
+    def SetR21(self, particle, X):
+        self.r21[particle] = X
+        
+    @staticmethod
+    def model(Z, N, norm, alpha, beta):
         return norm * np.exp(N*alpha + Z*beta)
 
-    def _model_err(self, Z, N, norm, alpha, beta, norm_err, alpha_err, beta_err):
-        return self.model(Z, N, norm, alpha, beta) * np.sqrt(N**2*alpha_err**2 + Z**2*beta_err**2 + norm_err**2/norm**2)
+    @staticmethod
+    def model_error(Z, N, norm, alpha, beta, norm_err, alpha_err, beta_err):
+        return Isoscaling.model(Z, N, norm, alpha, beta) * np.sqrt(N**2 * alpha_err**2 + Z**2 * beta_err**2 + norm_err**2 / norm**2)
 
-    # def ifit(self, range=(100, 400), drop_neutron=True, fixed_norm=False, norm=0.7, verbose=0):
 
-    #     data = dict()
-    #     for i, (pn, df) in enumerate(self.r21.items()):
-    #         zid = e15190.particle(pn).Z
-    #         nid = e15190.particle(pn).N
-    #         data[pn] = df.copy().query(f'x >= {range[0]} & x <= {range[1]}')
-    #         data[pn]['N'] = [nid] * data[pn].shape[0]
-    #         data[pn]['Z'] = [zid] * data[pn].shape[0]
+    def _fit(self, X, init_norm=1.0, init_alpha=0.5, init_beta=-0.5, **kwargs):
+        """ isoscaling fit for one Pt 
+        Parameters
+        ----------
+        X : np.ndarray
+            X[0] : [(Z,N)]
+            X[1] : y
+            X[2] : yerr
+        """
+        kw = {
+            'ncall' : None, 
+            'iterate' : 5, 
+            'verbose' : 1,
+            'limits' : [[0.8,1.2],[None,None],[None,None]],
+            'fix' : [True, False, False],
+        }
+        kw.update(kwargs)
 
-    #     data = pd.concat(list(data.values()), ignore_index=True)
+        def cost_fcn(norm, alpha, beta):
+            d2 = (X[1] - Isoscaling.model(*(np.array(list(X[0])).transpose()), norm, alpha, beta)) ** 2
+            e2 = X[2] ** 2
+            return np.sum(np.divide(d2, e2, where=(e2!=0.)))
+        
+        minuit = iminuit.Minuit(cost_fcn, norm=init_norm, alpha=init_alpha, beta=init_beta)
 
-    #     # models[pn] = lambda pn, norm, alpha, beta: self.model(zid, nid, norm, alpha, beta)
+        minuit.limits = kw['limits']
+        for i, para in enumerate(minuit.params):
+            minuit.fixed[para.name] = kw['fix'][i]
 
-    #     # cost_fcns[pn] = iminuit.cost.LeastSquares(df.x, df.y, df['y_eff'], self.m)
+        minuit.print_level = kw['verbose']
+        minuit.migrad(kw['ncall'], kw['iterate'])
+        minuit.hesse()
+        return minuit
 
-    #     # minuit = iminuit.Minuit(fcn, )
+    def fit(self, X, method:Literal['iterate','free']='iterate', norm_step_size=0.01, norm_range=(0.7,1.2)):
+        """ performa a global isoscaling fit for all data points
+        X : np.ndarray
+            X[0] : [(Z, N)]
+            X[1] : [[y]]
+            X[2] : [[yerr]]
 
-    # fit using TMinuit
+        method : str
+            `iterate` : fix `norm` across all points, repeat the fit for differenet `norm`
+            `free` : do not fix `norm`, fit each data independently
+        step_size : float
+            used when `method=iterate`, step size varying `norm`.
+        range : tuple of float
+            used when `method=iterate`, range of `norm`.
+        """
 
-    def fit(self, particles=None, range=(100, 400), drop_neutron=True, fixed_norm=False, norm=0.7, verbose=0):
+        if method == 'iterate':
+            minimum_chi2 = 1e10
+            minimum_norm = norm_range[0]
+
+            for n in np.arange(*norm_range, norm_step_size):
+                chi2 = np.sum([
+                    self._fit(
+                        X = np.array([list(X[0]), X[1][i], X[2][i]], dtype=object),
+                        init_norm = n,
+                        fix = [True, False, False]
+                    ).fval for i in range(X[1].shape[0])
+                ])
+                if chi2 < minimum_chi2:
+                    minimum_chi2 = chi2
+                    minimum_norm = n
+            
+        elif method == 'free':
+            minimum_norm = norm_range[0]
+
+        # do the global fit using the best normalization
+        minuit_collection = [
+            self._fit(
+                X = np.array([list(X[0]), X[1][i], X[2][i]], dtype=object),
+                init_norm = minimum_norm, 
+                fix = [method != 'free', False, False]
+            ) for i in range(X[1].shape[0])
+        ]
+
+        # saving result
+        self.chi2 = np.array(m.fval for m in minuit_collection)
+        self.param_names = minuit_collection[0].parameters
+        param_values = np.array([m.values for m in minuit_collection]).transpose()
+        param_errors = np.array([m.errors for m in minuit_collection]).transpose()
+        for i, name in enumerate(self.param_names):
+            self.param_values[name] = param_values[i]
+            self.param_errors[name] = param_errors[i]
+        
+        self.fitted = True
+        return self
+
+    def predict(self, X):
+        """ Calculate isoscaling R21 for a particle with proton Z and neutron N
+        X : Z, N
+        """
+        y = Isoscaling.model(*X, self.param_values['norm'], self.param_values['alpha'], self.param_values['beta'])
+        yerr = Isoscaling.model_error(*X, self.param_values['norm'], self.param_values['alpha'], self.param_values['beta'], self.param_errors['norm'], self.param_errors['alpha'], self.param_errors['beta'])
+        return y, yerr
+    
+    def fit_TMinuit(self, particles=None, range=(100, 400), drop_neutron=True, fixed_norm=False, norm=0.7, verbose=0):
+        """ Fit Isoscaling using TMinuit
+        """
         global fcn
         global subdf
 
@@ -52,11 +140,11 @@ class Isoscaling:
 
         data = dict()
         for particle in particles:
-            data[particle] = self.r21[particle].df.copy()
+            data[particle] = self.r21[particle].copy()
             data[particle].query(
                 f'x >= {range[0]} & x <= {range[1]}', inplace=True)
-            data[particle][['N', 'Z']] = e15190.particle(
-                particle).N, e15190.particle(particle).Z
+            data[particle][['N', 'Z']] = e15190.Particle(
+                particle).N, e15190.Particle(particle).Z
 
         if drop_neutron and 'n' in data:
             data.pop('n')
@@ -66,7 +154,7 @@ class Isoscaling:
         def fcn(npar, gin, f, par, iflag):
             chisq = 0.
             delta = (
-                subdf.y - self.model(subdf['Z'], subdf['N'], par[0], par[1], par[2])) / subdf['y_err']
+                subdf.y - Isoscaling.model(subdf['Z'], subdf['N'], par[0], par[1], par[2])) / subdf['y_err']
             chisq += np.sum(delta**2)
             f.value = chisq
 
@@ -86,8 +174,6 @@ class Isoscaling:
             gMinuit.set_parameter(2, 'beta', 0.5, 0.01)
             gMinuit.fit(verbose=verbose)
 
-            # getters = [gMinuit.get_parameter, gMinuit.get_error]
-            # names = [gMinuit.get_parameter_name(n) for n in range(3)]
             self.norm.append(gMinuit.get_parameter('norm'))
             self.alpha.append(gMinuit.get_parameter('alpha'))
             self.beta.append(gMinuit.get_parameter('beta'))
@@ -109,7 +195,7 @@ class Isoscaling:
         chisq_ = 1e5
         norm_ = norm_range[0]
         for x in np.arange(*norm_range, norm_step):
-            self.fit(particles=particles,
+            self.fit_TMinuit(particles=particles,
                      range=range, drop_neutron=drop_neutron, fixed_norm=True, norm=x)
             if self.chisq < chisq_:
                 norm_ = x
@@ -117,14 +203,17 @@ class Isoscaling:
 
         return norm_
 
-    def predict(self, particle):
-        df = self.r21[particle].df.copy()
+
+
+
+    def predict_TMinuit(self, particle):
+        df = self.r21[particle].copy()
         df.query(
             f'x >= {self.range_in_fit[0]} & x <= {self.range_in_fit[1]}', inplace=True)
 
-        y = self.model(e15190.particle(particle).Z, e15190.particle(
+        y = Isoscaling.model(e15190.Particle(particle).Z, e15190.Particle(
             particle).N, self.norm, self.alpha, self.beta)
-        yerr = self.model_err(e15190.particle(particle).Z, e15190.particle(
+        yerr = Isoscaling.model_error(e15190.Particle(particle).Z, e15190.Particle(
             particle).N, self.norm, self.alpha, self.beta, self.norm_err, self.alpha_err, self.beta_err)
         return pd.DataFrame({
             'x': df.x.to_numpy(),
@@ -134,19 +223,21 @@ class Isoscaling:
                 yerr, y, where=(y != 0.0), out=np.zeros_like(yerr))
         })
 
-    def plotIsoscaling(self, ax=None, particle=None, **kwargs):
-        if particle is None:
-            raise ValueError('particle not given')
 
-        df = self.predict(particle)
-        if ax is None:
-            ax = plt.gca()
-        ax.fill_between(df.x, df.y-df['y_err'], df.y+df['y_err'], **kwargs)
-        # ax.errorbar(df.x, df.y, yerr=df['y_err'], **kwargs)
-        return ax
+
+    # def plotIsoscaling(self, ax=None, particle=None, **kwargs):
+    #     if particle is None:
+    #         raise ValueError('particle not given')
+
+    #     df = self.predict(particle)
+    #     if ax is None:
+    #         ax = plt.gca()
+    #     ax.fill_between(df.x, df.y-df['y_err'], df.y+df['y_err'], **kwargs)
+    #     # ax.errorbar(df.x, df.y, yerr=df['y_err'], **kwargs)
+    #     return ax
 
     def plotChemPotentialAlpha(self, ax=None, **kwargs):
-        df = self.r21['p'].df.copy()
+        df = self.r21['p'].copy()
         df.query(
             f'x >= {self.range_in_fit[0]} & x <= {self.range_in_fit[1]}', inplace=True)
         x = df.x.to_numpy()
@@ -160,7 +251,7 @@ class Isoscaling:
         return df_helper.plot1d(ax, df, **kwargs)
 
     def plotChemPotentialBeta(self, ax=None, **kwargs):
-        df = self.r21['p'].df.copy()
+        df = self.r21['p'].copy()
         df.query(
             f'x >= {self.range_in_fit[0]} & x <= {self.range_in_fit[1]}', inplace=True)
         x = df.x.to_numpy()
