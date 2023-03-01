be46f970bc721e6d6a39fc0ff78744cfbcdead03
tck199732 <ctb8135@wmich.edu>
2023-02-28 22:00:51 -0500

    :construction: rewrite filter program

--- bin/filter_e15190.cpp
+++ bin/filter_e15190.cpp
@@ -4,7 +4,7 @@ struct AMD
 {
     const static int MAX_MULTI = 128;
     // base
-    int multi, Nc;
+    int multi;
     double b;
     std::array<int, MAX_MULTI> N;
     std::array<int, MAX_MULTI> Z;
@@ -12,24 +12,11 @@ struct AMD
     std::array<double, MAX_MULTI> py;
     std::array<double, MAX_MULTI> pz;
 
-    // // table21
-    // std::array<double, MAX_MULTI> ENG;
-    // std::array<double, MAX_MULTI> LANG;
-    // std::array<double, MAX_MULTI> JX;
-    // std::array<double, MAX_MULTI> JY;
-    // std::array<double, MAX_MULTI> JZ;
-
     // table3
     std::array<double, MAX_MULTI> J;
     std::array<double, MAX_MULTI> M;
     std::array<double, MAX_MULTI> WEIGHT;
     std::array<int, MAX_MULTI> iFRG;
-
-    // // table21t
-    // std::array<double, MAX_MULTI> t;
-    // std::array<double, MAX_MULTI> x;
-    // std::array<double, MAX_MULTI> y;
-    // std::array<double, MAX_MULTI> z;
 };
 struct E15190
 {
@@ -44,10 +31,6 @@ struct E15190
     std::array<double, MAX_MULTI> uball_px;
     std::array<double, MAX_MULTI> uball_py;
     std::array<double, MAX_MULTI> uball_pz;
-    std::array<double, MAX_MULTI> uball_J;
-    std::array<double, MAX_MULTI> uball_M;
-    std::array<double, MAX_MULTI> uball_WEIGHT;
-    std::array<int, MAX_MULTI> uball_iFRG;
 
     // hira
     int hira_multi;
@@ -56,10 +39,6 @@ struct E15190
     std::array<double, MAX_MULTI> hira_px;
     std::array<double, MAX_MULTI> hira_py;
     std::array<double, MAX_MULTI> hira_pz;
-    std::array<double, MAX_MULTI> hira_J;
-    std::array<double, MAX_MULTI> hira_M;
-    std::array<double, MAX_MULTI> hira_WEIGHT;
-    std::array<int, MAX_MULTI> hira_iFRG;
 
     // veto wall
     // neutron wall
@@ -78,12 +57,6 @@ void Initialize_TChain(TChain *&chain, const std::vector<std::string> &pths)
     chain->SetBranchAddress("py", &amd.py[0]);
     chain->SetBranchAddress("pz", &amd.pz[0]);
 
-    // table3
-    chain->SetBranchAddress("J", &amd.J[0]);
-    chain->SetBranchAddress("M", &amd.M[0]);
-    chain->SetBranchAddress("WEIGHT", &amd.WEIGHT[0]);
-    chain->SetBranchAddress("iFRG", &amd.iFRG[0]);
-
     chain->SetMakeClass(1);
     chain->SetBranchStatus("*", false);
     chain->SetBranchStatus("multi", true);
@@ -93,12 +66,6 @@ void Initialize_TChain(TChain *&chain, const std::vector<std::string> &pths)
     chain->SetBranchStatus("px", true);
     chain->SetBranchStatus("py", true);
     chain->SetBranchStatus("pz", true);
-
-    // table3
-    chain->SetBranchStatus("J", true);
-    chain->SetBranchStatus("M", true);
-    chain->SetBranchStatus("WEIGHT", true);
-    chain->SetBranchStatus("iFRG", true);
 
     for (auto &pth : pths)
     {
@@ -123,10 +90,6 @@ void Initialize_TTree(TTree *&tree)
     tree->Branch("uball_px", &filtered_amd.uball_px[0], "uball_px[uball_multi]/D");
     tree->Branch("uball_py", &filtered_amd.uball_py[0], "uball_py[uball_multi]/D");
     tree->Branch("uball_pz", &filtered_amd.uball_pz[0], "uball_pz[uball_multi]/D");
-    tree->Branch("uball_J", &filtered_amd.uball_J[0], "uball_J[uball_multi]/D");
-    tree->Branch("uball_M", &filtered_amd.uball_M[0], "uball_M[uball_multi]/D");
-    tree->Branch("uball_WEIGHT", &filtered_amd.uball_WEIGHT[0], "uball_WEIGHT[uball_multi]/D");
-    tree->Branch("uball_iFRG", &filtered_amd.uball_iFRG[0], "uball_iFRG[uball_multi]/D");
 
     // hira
     tree->Branch("hira_multi", &filtered_amd.hira_multi, "hira_multi/I");
@@ -135,33 +98,21 @@ void Initialize_TTree(TTree *&tree)
     tree->Branch("hira_px", &filtered_amd.hira_px[0], "hira_px[hira_multi]/D");
     tree->Branch("hira_py", &filtered_amd.hira_py[0], "hira_py[hira_multi]/D");
     tree->Branch("hira_pz", &filtered_amd.hira_pz[0], "hira_pz[hira_multi]/D");
-    tree->Branch("hira_J", &filtered_amd.hira_J[0], "hira_J[hira_multi]/D");
-    tree->Branch("hira_M", &filtered_amd.hira_M[0], "hira_M[hira_multi]/D");
-    tree->Branch("hira_WEIGHT", &filtered_amd.hira_WEIGHT[0], "hira_WEIGHT[hira_multi]/D");
-    tree->Branch("hira_iFRG", &filtered_amd.hira_iFRG[0], "hira_iFRG[hira_multi]/D");
 }
 
 int main(int argc, char *argv[])
 {
-    std::string reaction = argv[1];
-
-    std::string path_out = argv[2];
-    int narg = 3;
-    int number_files = std::stoi(argv[3]);
-    std::vector<std::string> input_pths;
-    for (int _ = 0; _ < number_files; _++)
-    {
-        input_pths.push_back(argv[++narg]);
-    }
+    ArgumentParser argparser(argc, argv);
 
     TChain *chain = new TChain("AMD");
-    Initialize_TChain(chain, input_pths);
+    Initialize_TChain(chain, argparser.input_files);
 
-    double betacms = Physics::GetReactionBeta(reaction);
-    double rapidity_beam = Physics::GetBeamRapidity(reaction); // not needed
+    double betacms = Physics::GetReactionBeta(argparser.reaction);
+    double rapidity_beam = Physics::GetBeamRapidity(argparser.reaction); // not needed
 
-    Microball *uball_detector = GetMicroBall(reaction);
-    HiRA *hira_detector = new HiRA();
+    Microball *microball = new Microball();
+    Initialize_MicroBall(microball, argparser.reaction);
+    HiRA *hira = new HiRA();
 
     TTree *tree = new TTree("AMD", "");
     Initialize_TTree(tree);
@@ -170,62 +121,47 @@ int main(int argc, char *argv[])
     {
         chain->GetEntry(ievt);
 
-        uball_detector->ResetCsI();
-        hira_detector->ResetCounter();
+        microball->ResetCsIHitMap();
+        hira->ResetCounter();
         for (unsigned int i = 0; i < amd.multi; i++)
         {
-            int A = amd.N[i] + amd.Z[i];
-            double px = amd.px[i] * A;
-            double py = amd.py[i] * A;
-            double pz_cms = amd.pz[i] * A;
-
-            particle particle = {amd.N[i], amd.Z[i], px, py, pz_cms};
+            particle particle = {amd.N[i], amd.Z[i], amd.px[i], amd.py[i], amd.pz[i]};
             particle.initialize(betacms);
+            correct_phi_value(particle, microball);
 
-            int uball_multi = uball_detector->GetCsIHits();
-            int hira_multi = hira_detector->GetCountPass();
+            int uball_multi = microball->GetCsIHits();
+            int hira_multi = hira->GetCountPass();
 
-            if (ReadMicroballParticle(uball_detector, particle))
+            if (ReadMicroballParticle(microball, particle))
             {
-                filtered_amd.uball_N[uball_multi] = amd.N[i];
-                filtered_amd.uball_Z[uball_multi] = amd.Z[i];
-                filtered_amd.uball_px[uball_multi] = px;
-                filtered_amd.uball_py[uball_multi] = py;
-                filtered_amd.uball_pz[uball_multi] = pz_cms;
+                filtered_amd.uball_N[uball_multi] = particle.N;
+                filtered_amd.uball_Z[uball_multi] = particle.Z;
+                filtered_amd.uball_px[uball_multi] = particle.px;
+                filtered_amd.uball_py[uball_multi] = particle.py;
+                filtered_amd.uball_pz[uball_multi] = particle.pz_lab;
 
-                // table3
-                filtered_amd.uball_J[uball_multi] = amd.J[i];
-                filtered_amd.uball_M[uball_multi] = amd.M[i];
-                filtered_amd.uball_WEIGHT[uball_multi] = amd.WEIGHT[i];
-                filtered_amd.uball_iFRG[uball_multi] = amd.iFRG[i];
-
-                uball_detector->AddCsIHit(particle.theta_lab, particle.phi);
+                microball->AddCsIHit(particle.theta_lab, particle.phi);
             }
 
-            if (ReadHiRAParticle(hira_detector, particle))
+            if (ReadHiRAParticle(hira, particle))
             {
-                filtered_amd.hira_N[hira_multi] = amd.N[i];
-                filtered_amd.hira_Z[hira_multi] = amd.Z[i];
-                filtered_amd.hira_px[hira_multi] = px;
-                filtered_amd.hira_py[hira_multi] = py;
-                filtered_amd.hira_pz[hira_multi] = pz_cms;
+                filtered_amd.hira_N[hira_multi] = particle.N;
+                filtered_amd.hira_Z[hira_multi] = particle.Z;
+                filtered_amd.hira_px[hira_multi] = particle.px;
+                filtered_amd.hira_py[hira_multi] = particle.py;
+                filtered_amd.hira_pz[hira_multi] = particle.pz_lab;
 
-                // table3
-                filtered_amd.hira_J[hira_multi] = amd.J[i];
-                filtered_amd.hira_M[hira_multi] = amd.M[i];
-                filtered_amd.hira_WEIGHT[hira_multi] = amd.WEIGHT[i];
-                filtered_amd.hira_iFRG[hira_multi] = amd.iFRG[i];
-                hira_detector->CountPass();
+                hira->CountPass();
             }
         }
 
-        filtered_amd.hira_multi = hira_detector->GetCountPass();
-        filtered_amd.uball_multi = uball_detector->GetCsIHits();
+        filtered_amd.hira_multi = hira->GetCountPass();
+        filtered_amd.uball_multi = microball->GetCsIHits();
         filtered_amd.b = amd.b;
         tree->Fill();
     }
 
-    TFile *outputfile = new TFile(path_out.c_str(), "RECREATE");
+    TFile *outputfile = new TFile(argparser.output_file.c_str(), "RECREATE");
     outputfile->cd();
     tree->Write();
     outputfile->Write();
--- bin/filter_e15190.hh
+++ bin/filter_e15190.hh
@@ -1,34 +1,51 @@
-#include "../src/Physics.cpp"
-#include "../src/Microball.cpp"
-#include "../src/HiRA.cpp"
+#include "Physics.cpp"
+#include "Microball.cpp"
+#include "HiRA.cpp"
 
 #include "TChain.h"
 #include "TFile.h"
 #include "TTree.h"
 
+#include <map>
 #include <vector>
 #include <string>
 #include <stdlib.h>
-#include <map>
+#include <getopt.h>
 #include <filesystem>
 namespace fs = std::filesystem;
 
-/**
- * @brief Input the N, Z and momenta (not per nucleon !)
- *
- */
 struct particle
 {
     int N, Z;
     double px, py, pz_cms;
-    double kinergy_lab;
+    double kinergy_lab, pz_lab;
     double theta_lab, phi;
     void initialize(const double &betacms);
 };
 
+std::map<std::array<int, 2>, double> AME_MASS_TABLE;
+void ReadAMETable(const std::string &filename, std::map<std::array<int, 2>, double> &ame_mass_table)
+{
+    std::ifstream infile(filename);
+
+    infile.ignore(99, '\n');
+    int Z, A;
+    double mass;
+    while (infile >> Z)
+    {
+        infile >> A >> mass;
+        ame_mass_table[{Z, A}] = mass;
+    }
+}
+
 void particle::initialize(const double &betacms)
 {
-    double mass = Physics::GetNucleiMass(this->Z, this->N + this->Z);
+    double mass = AME_MASS_TABLE[{this->Z, this->N + this->Z}];
+    double A = this->N + this->Z;
+    this->px = this->px * A;
+    this->py = this->py * A;
+    this->pz_cms = this->pz_cms * A;
+
     double gamma = 1. / TMath::Sqrt(1 - pow(betacms, 2.));
     double pmag_trans = TMath::Sqrt(pow(this->px, 2.) + pow(this->py, 2.));
     double pmag_cms = TMath::Sqrt(pow(pmag_trans, 2.) + pow(this->pz_cms, 2.));
@@ -41,62 +58,130 @@ void particle::initialize(const double &betacms)
     this->theta_lab = TMath::ATan2(pmag_trans, pz_lab) * TMath::RadToDeg();
 
     this->kinergy_lab = TMath::Sqrt(pow(pmag_lab, 2.) + pow(mass, 2.)) - mass;
-    this->phi = TMath::ATan2(this->py, this->px) * TMath::RadToDeg();
-
-    // uball coordinate phi=[-18,342]
-    if (this->phi < -18.)
-    {
-        this->phi += 360.;
-    }
-    if (this->phi > 342)
-    {
-        this->phi -= 360.;
-    }
+    this->phi = TMath::ATan2(this->py, this->px) * TMath::RadToDeg(); // from -180 to 180 here, need to convert to uball coordinate for each ring
 }
 
-Microball *GetMicroBall(const std::string &reaction)
+void Initialize_MicroBall(Microball *&microball, const std::string &reaction)
 {
-    Microball *uBall = new Microball();
     fs::path project_dir = std::getenv("PROJECT_DIR");
     fs::path database_dir = project_dir / "database/e15190/microball/acceptance";
     fs::path path_config = database_dir / "config.dat";
     fs::path path_geometry = database_dir / "geometry.dat";
+    fs::path path_threshold = database_dir / "fitted_threshold.dat";
 
-    std::map<int, fs::path> path_thres;
-    path_thres[2] = database_dir / "threshold_Sn_65mgcm2.dat";
-    path_thres[3] = database_dir / "threshold_Sn_58mgcm2.dat";
-    path_thres[4] = database_dir / "threshold_Sn_50mgcm2.dat";
-    path_thres[5] = database_dir / "threshold_Sn_43mgcm2.dat";
-    path_thres[7] = database_dir / "threshold_Sn_30mgcm2.dat";
-    path_thres[8] = database_dir / "threshold_Sn_23mgcm2.dat";
-
-    uBall->ReadGeometry(path_geometry.string());
-
-    for (auto &[id, pth] : path_thres)
-    {
-        if (!fs ::exists(pth))
-        {
-            std::string msg = Form("%s does not exists.", pth.c_str());
-            throw std::invalid_argument(msg.c_str());
-        }
-        uBall->ReadThreshold(id, pth.string());
-    }
-
-    uBall->ReadConfiguration(reaction, path_config.string());
-
-    uBall->HiRA_Coordinate(); // add phi angle by 90, and move to phi=[0,360];
-
-    return uBall;
+    microball->ConfigurateSetup(reaction, path_config.string());
+    microball->ReadGeometryMap(path_geometry.string());
+    microball->ReadThresholdKinergyMap(path_threshold.string());
 }
 
-bool ReadMicroballParticle(Microball *&uBall, const particle &particle)
+bool ReadMicroballParticle(Microball *&mb, const particle &part)
 {
-    return uBall->IsChargedParticle(particle.Z) &&
-           uBall->IsCovered(particle.theta_lab, particle.phi) &&
-           uBall->IsAccepted(particle.kinergy_lab, particle.theta_lab, particle.N + particle.Z, particle.Z) && uBall->IsReadyCsI(particle.theta_lab, particle.phi);
+    bool pass_charge = mb->IsChargedParticle(part.Z);
+    bool pass_coverage = mb->IsCovered(part.theta_lab, part.phi);
+    bool pass_threshold = mb->IsAccepted(part.kinergy_lab, part.theta_lab, part.N + part.Z, part.Z);
+    return pass_charge && pass_coverage && pass_threshold;
 }
 
 bool ReadHiRAParticle(HiRA *&hira, const particle &particle)
 {
     return hira->PassAngularCut(particle.theta_lab, particle.phi) && hira->PassCharged(particle.Z) && hira->PassKinergyCut(particle.Z + particle.N, particle.Z, particle.kinergy_lab);
 }
+
+void correct_phi_value(particle &part, Microball *&microball)
+{
+    int ring = microball->GetRingID(part.theta_lab);
+    if (ring == -1)
+    {
+        return;
+    }
+    double phi_min_in_ring = microball->GetPhiMinInRing(ring);
+    double phi_max_in_ring = microball->GetPhiMaxInRing(ring);
+
+    if (part.phi < phi_min_in_ring)
+    {
+        part.phi += 360;
+    }
+    if (part.phi > phi_max_in_ring)
+    {
+        part.phi -= 360;
+    }
+}
+
+class ArgumentParser
+{
+public:
+    // required arguments
+    std::string reaction;
+    std::vector<std::string> input_files;
+    std::string output_file;
+
+    ArgumentParser(int argc, char *argv[])
+    {
+        options = {
+            {"help", no_argument, 0, 'h'},
+            {"reaction", required_argument, 0, 'r'},
+            {"input", required_argument, 0, 'i'},
+            {"output", required_argument, 0, 'o'},
+            {0, 0, 0, 0},
+        };
+
+        int option_index = 0;
+        int opt;
+        while ((opt = getopt_long(argc, argv, "hr:i:o:", options.data(), &option_index)) != -1)
+        {
+            switch (opt)
+            {
+
+            case 'r':
+            {
+                this->reaction = optarg;
+                break;
+            }
+            case 'i':
+            {
+                std::string files = optarg;
+                std::string delimiter = " ";
+                size_t pos = 0;
+                std::string token;
+                while ((pos = files.find(delimiter)) != std::string::npos)
+                {
+                    token = files.substr(0, pos);
+                    this->input_files.push_back(token);
+                    files.erase(0, pos + delimiter.length());
+                }
+                if (files.length() > 0)
+                {
+                    this->input_files.push_back(files);
+                }
+                break;
+            }
+            case 'o':
+            {
+                this->output_file = optarg;
+                break;
+            }
+            case '?':
+            {
+                std::cout << "Got unknown option." << std::endl;
+                break;
+            }
+
+            case 'h':
+            {
+                this->help();
+            }
+            default:
+            {
+                std::cout << "Got unknown parse returns: " << optarg << std::endl;
+            }
+            }
+        }
+    }
+    void help()
+    {
+        std::cout << "" << std::endl;
+    }
+
+protected:
+    std::vector<option> options;
+};
