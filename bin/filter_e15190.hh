778a3ee0f31c4ef400a8f3aa0fb62b01d8c97c1b
tck199732 <ctb8135@wmich.edu>
2023-03-01 22:09:56 -0500

    :recycle: move progressbar to src

--- bin/filter_e15190.hh
+++ bin/filter_e15190.hh
@@ -1,6 +1,7 @@
 #include "Physics.cpp"
 #include "Microball.cpp"
 #include "HiRA.cpp"
+#include "ProgressBar.cpp"
 
 #include "TChain.h"
 #include "TFile.h"
@@ -216,35 +217,4 @@ public:
 
 protected:
     std::vector<option> options;
-};
-
-class ProgressBar
-{
-public:
-    int njobs;
-    int njobs_done;
-    std::string progress_name;
-    ProgressBar(const int &njobs, const std::string &name = "Progress")
-    {
-        this->progress_name = name;
-        this->njobs = njobs;
-        this->njobs_done = 0;
-    }
-    void Update()
-    {
-        if (this->njobs_done == this->njobs)
-        {
-            return;
-        }
-        this->njobs_done++;
-
-        if (this->njobs_done % (this->njobs / 100) == 0)
-        {
-            double progress = (double)njobs_done / njobs;
-            int barWidth = 70;
-            std::cout << Form("%14s: [", this->progress_name.c_str());
-            std::cout << std::string(barWidth * progress, '=') << std::string(barWidth * (1 - progress), ' ') << "] " << int(progress * 100.0) << "%\r";
-            std::cout << std::flush;
-        }
-    }
 };
