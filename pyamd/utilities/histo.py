204f234c6a0a19f2b4f9ab07372f5a60bd8d5c22
tck199732 <ctb8135@wmich.edu>
2023-04-21 06:47:39 -0400

    ✨ add basic operations of hist

--- pyamd/utilities/histo.py
+++ pyamd/utilities/histo.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-class histogram:
+class histogram_handler:
     @staticmethod
     def projection(df, cut=None, range=None, bins=None, axis='x', normalize=True):
         """ Project a 2D histogram (in terms of pd.DataFrame) along an axis
@@ -67,3 +67,139 @@ class histogram:
             'y_ferr' : np.divide(histerr, hist, out=np.zeros_like(hist), where=hist!=0)
         })
 
+    @staticmethod
+    def rebin1D(df, range, bins=None, normalize=True):
+        df = df.copy()
+        if df.shape[1] > 4:
+            raise ValueError('Dataframe must have at most 4 columns')
+        elif df.shape[1] == 4:
+            df.columns = ['x', 'y', 'y_err', 'y_ferr']
+        elif df.shape[1] == 3:
+            df.columns = ['x', 'y', 'y_err']
+        elif df.shape[1] == 2:
+            df.columns = ['x', 'y']
+            df['y_err'] = 0
+        else:
+            raise ValueError('Dataframe must have at least 2 columns')
+        
+        bins = df.shape[0] if bins is None else bins
+        hist, edges = np.histogram(
+            df['x'],
+            weights=df['y'],
+            range=range,
+            bins=bins
+        )
+
+        histerr, _ = np.histogram(
+            df['x'],
+            weights=df['y_err']**2,
+            range=range,
+            bins=bins
+        )
+        histerr = np.sqrt(histerr)
+        norm = (np.diff(range) / bins)[0] if normalize else 1.
+
+        return pd.DataFrame({
+            'x' : (edges[1:] + edges[:-1])/2,
+            'y' : hist / norm,
+            'y_err' : histerr / norm,
+            'y_ferr' : np.divide(histerr, hist, out=np.zeros_like(hist), where=hist!=0)
+        })
+
+    @staticmethod
+    def mul(a, b, axis='y'):
+        """ Multiply two 1D histograms (in terms of pd.DataFrame). Currently only implemented for 1D histograms as we rarely multiply multi-dimensional histograms.
+        Parameters
+        ----------
+        a : pd.DataFrame
+            Dataframe containing the first histogram.
+        b : pd.DataFrame
+            Dataframe containing the second histogram.
+        axis : str, optional    
+            Axis along which the multiplication is performed, depending on the name of the columns. Default is 'y'.
+        Returns
+        -------
+        pd.DataFrame
+            Dataframe containing the product of the two histograms.
+        """
+        if not set(['x', 'y', 'y_err']).issubset(set(a.columns)) and set(['x', 'y', 'y_err']).issubset(set(b.columns)):
+            raise ValueError('Dataframes must have at least 3 columns')
+
+        return pd.DataFrame({
+            'x' : a['x'],
+            axis : a[axis] * b[axis],
+            f'{axis}_err' : np.sqrt((a[f'{axis}_err'] * b[axis])**2 + (a[axis] * b[f'{axis}_err'])**2),
+            f'{axis}_ferr' : np.sqrt((a[f'{axis}_ferr'] * b[axis])**2 + (a[axis] * b[f'{axis}_ferr'])**2)
+        })
+
+    @staticmethod
+    def div(a, b, axis='y'):
+        """ Divide two 1D histograms (in terms of pd.DataFrame). Currently only implemented for 1D histograms as we rarely divide multi-dimensional histograms.
+        Parameters
+        ----------
+        a : pd.DataFrame
+            Dataframe containing the first histogram.
+        b : pd.DataFrame
+            Dataframe containing the second histogram.
+        axis : str, optional
+            Axis along which the division is performed, depending on the name of the columns. Default is 'y'.
+        Returns
+        -------
+        pd.DataFrame
+            Dataframe containing the quotient of the two histograms.
+        """
+        if not set(['x', 'y', 'y_err']).issubset(set(a.columns)) and set(['x', 'y', 'y_err']).issubset(set(b.columns)):
+            raise ValueError('Dataframes must have at least 3 columns')
+        
+        if not 'y_ferr' in a.columns:
+            a['y_ferr'] = np.divide(a['y_err'], a['y'], out=np.zeros_like(a['y']), where=a['y']!=0)
+        if not 'y_ferr' in b.columns:
+            b['y_ferr'] = np.divide(b['y_err'], b['y'], out=np.zeros_like(b['y']), where=b['y']!=0)
+
+        y = np.divide(a[axis], b[axis], out=np.zeros_like(a[axis]), where=b[axis]!=0)
+        y_err = y * np.sqrt(a[f'{axis}_ferr'] ** 2. + b[f'{axis}_ferr'] ** 2.)
+        return pd.DataFrame({
+            'x' : a['x'],
+            axis : y,
+            f'{axis}_err' : y_err,
+            f'{axis}_ferr' : np.divide(y_err, y, out=np.zeros_like(y), where=y!=0)
+        })
+
+    @staticmethod
+    def sum(a, b, axis='y', sign=1.):
+        """ Get the sum of two 1D histograms (in terms of pd.DataFrame). Currently only implemented for 1D histograms as we rarely add multi-dimensional histograms.
+        Parameters
+        ----------
+        a : pd.DataFrame
+            Dataframe containing the first histogram.
+        b : pd.DataFrame
+            Dataframe containing the second histogram.
+        axis : str, optional
+            Axis along which the addition is performed, depending on the name of the columns. Default is 'y'.
+        sign : float, optional
+            Sign of the second histogram. Default is 1. 
+        Returns
+        -------
+        pd.DataFrame
+            Dataframe containing the sum of the two histograms.
+        """
+
+        if not set(['x', 'y', 'y_err']).issubset(set(a.columns)) and set(['x', 'y', 'y_err']).issubset(set(b.columns)):
+            raise ValueError('Dataframes must have at least 3 columns')
+        
+        y = a[axis] + b[axis] * sign
+        y_err = np.sqrt(a[f'{axis}_err']**2 + b[f'{axis}_err']**2)
+        return pd.DataFrame({
+            'x' : a['x'],
+            axis : y,
+            f'{axis}_err' : y_err,
+            f'{axis}_ferr' : np.divide(y_err, y, out=np.zeros_like(y), where=y!=0)
+        })
+    
+    @staticmethod
+    def add(a, b, axis='y'):
+        return histogram.sum(a, b, axis, sign=1.)
+    
+    @staticmethod
+    def sub(a, b, axis='y'):
+        return histogram.sum(a, b, axis, sign=-1.)
